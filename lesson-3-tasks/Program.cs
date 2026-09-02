9abc7e06d9658fa91318fffd290bf51bb9162587
VonRiddarn <vonriddarn@gmail.com>
2026-09-02 23:04:54 +0200

    feat: add excercise 2 and comment out E1 code

--- lesson-3-tasks/Program.cs
+++ lesson-3-tasks/Program.cs
@@ -5,7 +5,7 @@
 /*
  * Exercise 1
  * 
- * "Tänk dig ett program som ber användaren mata in två tal, adderar dem och skriver ut resultatet"
+ * Tänk dig ett program som ber användaren mata in två tal, adderar dem och skriver ut resultatet.
  * 
  * Get input A
  * Parse to int
@@ -20,25 +20,45 @@
  * Print (A + B)
 */
 
-int _a;
-int _b;
+// int _a;
+// int _b;
 
-while (true)
-{
-	Console.Write("Enter input A: ");
-	if (int.TryParse(Console.ReadLine(), out _a))
-		break;
-	else
-		Console.WriteLine("Failed. Input must be an integer.");
-}
+// while (true)
+// {
+// 	Console.Write("Enter input A: ");
+// 	if (int.TryParse(Console.ReadLine(), out _a))
+// 		break;
+// 	else
+// 		Console.WriteLine("Failed. Input must be an integer.");
+// }
 
-while (true)
-{
-	Console.Write("Enter input B: ");
-	if (int.TryParse(Console.ReadLine(), out _b))
-		break;
-	else
-		Console.WriteLine("Failed. Input must be an integer.");
-}
+// while (true)
+// {
+// 	Console.Write("Enter input B: ");
+// 	if (int.TryParse(Console.ReadLine(), out _b))
+// 		break;
+// 	else
+// 		Console.WriteLine("Failed. Input must be an integer.");
+// }
 
-Console.WriteLine($"Sum: {_a + _b}");
+// Console.WriteLine($"Sum: {_a + _b}");
+
+/*
+ * Excercise 2
+ * 
+ * Skriv programmet för denna pseudokod
+		SKAPA en variabel som heter SUMMA och sätt den till 0
+		LOOPA oändligt
+			SKAPA en tom sträng som heter INPUT
+			LÄS in en rad från användaren och spara i INPUT
+			OM INPUT är lika med "sluta" (case insensitive)
+				AVBRYT loopen
+			ANNARS OM INPUT är tom
+				SKRIV UT "Du måste mata in något!"
+			ANNARS OM INPUT går att tolka som ett heltal
+				ADDERA heltalet till SUMMA
+			ANNARS
+				SKRIV UT "Det där var inte ett giltigt tal!"
+		SKRIV UT "Summan av talen är: " + SUMMA
+*/
+

4d4276aa178bc0b86b0a550830cb6ba0dfa08e35
VonRiddarn <vonriddarn@gmail.com>
2026-09-02 23:03:07 +0200

    feat: add lesson 3 and first pseudo code assignment

---
+++ lesson-3-tasks/Program.cs
@@ -0,0 +1,44 @@
+﻿// ----- ----- -----
+// 	  Pseudocode
+// ----- ----- -----
+
+/*
+ * Exercise 1
+ * 
+ * "Tänk dig ett program som ber användaren mata in två tal, adderar dem och skriver ut resultatet"
+ * 
+ * Get input A
+ * Parse to int
+ * IF FAIL:
+ * 		PRINT FAIL
+ * 		RETRY
+ * Get input B
+ * Parse to int
+ * IF FAIL:
+ * 		PRINT FAIL
+ * 		RETRY
+ * Print (A + B)
+*/
+
+int _a;
+int _b;
+
+while (true)
+{
+	Console.Write("Enter input A: ");
+	if (int.TryParse(Console.ReadLine(), out _a))
+		break;
+	else
+		Console.WriteLine("Failed. Input must be an integer.");
+}
+
+while (true)
+{
+	Console.Write("Enter input B: ");
+	if (int.TryParse(Console.ReadLine(), out _b))
+		break;
+	else
+		Console.WriteLine("Failed. Input must be an integer.");
+}
+
+Console.WriteLine($"Sum: {_a + _b}");

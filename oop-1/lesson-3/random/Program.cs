9b61aa6dd44f74a247c1d5f65eceec138f415ee7
VonRiddarn <vonriddarn@gmail.com>
2026-09-05 14:54:15 +0200

    feat: add excercise 1 for "random"

---
+++ oop-1/lesson-3/random/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Excercise 1
+ * 
+ * Slumpa ett tal mellan 1–6 och säg vad tärningskastet blev.
+ * Utveckling: Slumpa två tärningar, visa båda resultaten och summan.
+*/
+
+int _die1 = Random.Shared.Next(1, 7);
+int _die2 = Random.Shared.Next(1, 7);
+
+Console.WriteLine($"You rolled: {_die1}.");
+
+_die1 = Random.Shared.Next(1, 7);
+
+int _sum = _die1 + _die2;
+string statePastTense = _sum == 7 ? "won" : "lost";
+
+Console.WriteLine($"You rolled: {_die1} and {_die2} ({_sum}). You {statePastTense}.");

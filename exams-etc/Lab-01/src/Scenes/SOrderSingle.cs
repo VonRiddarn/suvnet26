633f0659825fba0f7f7cfe6f43867e12c2f42fb7
VonRiddarn <vonriddarn@gmail.com>
2026-09-10 12:04:59 +0200

    feat: implement test menu for single order

--- exams-etc/Lab-01/src/Scenes/SOrderSingle.cs
+++ exams-etc/Lab-01/src/Scenes/SOrderSingle.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using ConsoleAtHome;
+
 class SOrderSingle : IScene
 {
 	public void Enter() { }
@@ -6,7 +9,38 @@ class SOrderSingle : IScene
 
 	public ExitContext Run()
 	{
-		Console.WriteLine("Ayo, wassup! Imma head back to the menu once you press ENTER dawg.");
+		string name;
+		decimal kilos;
+		decimal value;
+		bool member;
+		bool wantInsurance;
+
+		StringBuilder accumulativeMenu = new("== Lasses last 1.0 || ENSKILD FRAKT\n");
+		Console.Clear();
+		Console.WriteLine(accumulativeMenu.ToString());
+
+		// TODO: DRY this up later, obv.
+		// Either through a repeatable method, 
+		// or by using some dynamic wizard that iterates through properties
+
+		name = Cah.Input.ReadLine("Namn: ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Namn: {name}\n").ToString());
+		kilos = Cah.Input.ParseLine<decimal>("Vikt (kg): ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Vikt: {kilos} kg\n").ToString());
+		value = Cah.Input.ParseLine<decimal>("Value (SEK): ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Value: {value} SEK\n").ToString());
+		member = Cah.Input.ParseYesNo("Är du förmånsmedlem genom Lasse++ ? (y/n): ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Medlem: {(member ? "Ja" : "Nej")}\n").ToString());
+		wantInsurance = Cah.Input.ParseYesNo("Vill du försäkra ditt paket? (y/n): ");
+		Console.Clear();
+		Console.Write(accumulativeMenu.Append($"Försäkra: {(wantInsurance ? "Ja" : "Nej")}\n").ToString());
+
+		// This wont do jack all atm.
+		Console.WriteLine("Stämmer detta? (ENTER)");
 		Console.ReadLine();
 
 		return new(null);

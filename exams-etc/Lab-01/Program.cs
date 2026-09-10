d4bf87714ffb60b5962084ae6b57b798dea3dddc
VonRiddarn <vonriddarn@gmail.com>
2026-09-10 10:26:26 +0200

    docs: update notes

--- exams-etc/Lab-01/Program.cs
+++ exams-etc/Lab-01/Program.cs
@@ -19,6 +19,9 @@ Detta är pga tidspress. Tror att ett genomtänkt DI system hade varit bättre.
 Alternativet hade varit att göra scener till objekt istället för interfaces och passera ned repo och manager.
 Då hade man också kunnat utveckla sub-scener / context.
 
+Tekniskt sett hade vi enkelt kunnat förstöra detta systemet genom att bara spamma "keepOld" på alla menyer.
+Just nu kollar vi inte om den nuvarande menyn bör vara unik osv. Det är medvetna trade-offs jag gjorde pga tidspress. ):
+
 
 10.19 : Fungerande state machine för menyer!!! Lesfuggin goo
  */
